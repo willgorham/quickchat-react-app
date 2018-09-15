9b214ca4e0a4392d00fc0e96aaba9c9cde315bae
Will Gorham <willgorham@gmail.com>
2018-09-15 14:37:20 -0400

    MessageList component with dummy data

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
+const DUMMY_DATA = [
+  {
+    senderId: "perborgen",
+    text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "Brazil!"
+  }
+]
+
 class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      messages: DUMMY_DATA
+    }
+  }
 
   render() {
     return (
       <div className="app">
         <Title />
-        <MessageList />
+        <MessageList messages={this.state.messages} />
         <SendMessageForm />
       </div>
     )
   }
 
 }
+
+class Title extends React.Component {
+
+  render() { return null; }
+}
+
+
+class MessageList extends React.Component {
+
+  render() {
+    return (
+      <ul className="message-list">
+        {this.props.messages.map(message => {
+          return (
+            <li key={message.id}>
+              <div>
+                {message.senderId}
+              </div>
+              <div>
+                {message.text}
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+}
+
+class SendMessageForm extends React.Component {
+
+  render() { return null; }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
