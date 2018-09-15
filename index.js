ab3b31037c4d9fd20f2dbdd01c8f880507288916
Will Gorham <willgorham@gmail.com>
2018-09-15 14:04:35 -0400

    Create root App component

---
+++ index.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="app">
+        <Title />
+        <MessageList />
+        <SendMessageForm />
+      </div>
+    )
+  }
+
+}
